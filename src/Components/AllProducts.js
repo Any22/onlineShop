e8a299383fc37f8cf3fd0f1c385ec99a3e00eb35
Saeed Ahmed <sahmed@c-cor.local>
2021-07-11 00:39:23 +1000

    added buttons and made the display responsive for various devices

--- src/Components/AllProducts.js
+++ src/Components/AllProducts.js
@@ -1,16 +1,73 @@
 import React from 'react'
+import styled from 'styled-components';
+
 
 const AllProducts = ({products}) => {
     return (
+      <Wrapper>
        <div className="allProducts">
        {products.map((product)=>( 
-        <div key={product.id}>
-          <h3>{product.title}</h3>
-          <img src={product.image} alt={product.title}/>
-        <p>{product.description}</p>   
+        <div className="singleProduct" key={product.id}>
+          <h4>{product.title}</h4>
+          <img src={product.image} alt={product.title}/> 
+          <h4> ${product.price}</h4>
+          <button id="addToCart">Add to cart</button>
+          <div className="spaceBetweenButtons"></div>
+          <button id="productDetail">Product details</button>
         </div>
        ))}
     </div> 
+    </Wrapper>
     )
 }
+const Wrapper = styled.section`
+  img {
+    height: 175px;
+  }
+  .products-container {
+    display: grid;
+    grid-template-columns: auto auto ;
+    grid-gap:20px;
+  }
+
+  .singleProduct{
+    border : #d3d3d3 solid 2px;
+    padding: 20px 0px;
+    font-size: 14px;
+    text-align: center;
+}
+
+  button{
+    border-radius: 14px;
+    background-color:#4c8bf8;
+    color:white;
+    border:#4c8bf8;
+    padding:8px;
+  }
+.spaceBetweenButtons
+  {
+    width:16px;
+    height:auto;
+    display:inline-block;
+}
+// Adding responsiveness
+  @media(min-width:350px){
+    .products-container {
+      grid-template-rows: repeat(1, 1fr);
+  }
+  .singleProduct{
+    padding:20px 0px;
+  }
+}
+  @media (min-width: 992px) {
+    .products-container {
+      grid-template-rows: repeat(2, 1fr);
+    }
+  }
+  @media (min-width: 1170px) {
+    .products-container {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+`
  export default AllProducts;
