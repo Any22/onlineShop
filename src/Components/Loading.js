ef9948dd143c951858c7cce458982f48b33cb2b1
Saba Akhtar <sabasaeedakhtar@gmail.com>
2021-07-16 14:30:31 +1000

    changed the css property to make Loading text appear in the middle of page

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 const Loading =() => {
     return (
-    <div className="loading">
-      <h2>Loading.......! </h2>
-    </div>
+      <Wrapper>
+        <div className="section-center">
+        <h2>Loading.......! </h2>
+        </div>
+      </Wrapper>
     );
 
   
 };
-
+const Wrapper = styled.section`
+margin-top:350px;
+text-align: center;
+`
 export default Loading;
