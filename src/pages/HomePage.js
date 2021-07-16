66d948c1eb057530158bad9fc29f08edef6c6cd4
Saba Akhtar <sabasaeedakhtar@gmail.com>
2021-07-16 14:31:34 +1000

    created home and products pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HomePage = () => {
+    return (
+        <Wrapper>
+        <div className="section-center">
+        <h2>Welcome page </h2>   
+        </div>
+        </Wrapper>
+    );
+}
+const Wrapper = styled.section`
+margin-top:350px;
+text-align: center;
+`
+ export default HomePage ;
