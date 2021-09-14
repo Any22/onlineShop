cab594344af6e9ddce656f2168c2fb4596a616ee
Saba Akhtar <sabasaeedakhtar@gmail.com>
2021-09-14 19:30:10 +1000

    added image slider on home page

---
+++ src/utilities/sliderImages.js
@@ -0,0 +1,17 @@
+import image1 from '../Images/rings.jpg';
+import image2 from '../Images/electronics.jpg';
+import image3 from '../Images/home.jpg';
+import image4 from '../Images/men.jpeg';
+import image5 from '../Images/women.jpg';
+import image6 from '../Images/home.jpg';
+
+const images = [
+      
+    <img src={image3} alt="" className="sliderimg"/>,
+    <img src={image2} alt="" className="sliderimg"/>,
+    <img src={image1} alt="" className="sliderimg"/>,
+    <img src={image4} alt="" className="sliderimg"/>,
+    <img src={image5} alt="" className="sliderimg"/>,
+    <img src={image6} alt="" className="sliderimg"/>,
+]
+export default images;
